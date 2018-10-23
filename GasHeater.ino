279193e2894b23af75950eb039850739a62bf78a
AndrewPetrov <andrew.petrov.dev@gmail.com>
2018-10-23 20:26:54 +0300

    all works

--- GasHeater.ino
+++ GasHeater.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Thread.h>
+#include <avr/wdt.h>
 
 // which analog pin to connect
 #define THERMISTORPIN A0
@@ -12,14 +13,14 @@
 #define TEMPERATURENOMINAL 25
 // how many samples to take and average, more takes longer
 // but is more 'smooth'
-#define NUMSAMPLES 20
+#define NUMSAMPLES 100
 // The beta coefficient of the thermistor (usually 3000-4000)
 #define BCOEFFICIENT 3950
 //#define BCOEFFICIENT 3000
 // the value of the 'other' resistor
 #define SERIESRESISTOR 97000
 
-uint16_t samples[NUMSAMPLES];
+int loop_count = 0;
 
 #define lcd1Light DD3
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
@@ -61,9 +62,11 @@ Servo servo;
 int servoPin = 7;
 
 int minAngle = 180;
+int startAngle = 120;
 int maxAngle = 90;
 int servoAngle = minAngle;   // servo position in degrees
 bool isServoAttached = false;
+bool isCycleSkipped = false;
 
 enum servoState {
   none,
@@ -81,6 +84,7 @@ double minTemp = 20.0;
 double targetTemp = 36.0;
 double currentTemp = 25.0;
 double threshold = 1.0;
+double previousTemp = 0.0;
 
 Thread temperatureAdjustingThread = Thread();
 Thread getCurrentTemperatureThread = Thread();
@@ -90,11 +94,9 @@ Thread lightDownThread = Thread();
 
 void lightUp() {
   if (lcdLevel < maxLevel) {
-    Serial.println(lcdLevel);
     currentLcdState = lcdUp;
-    lcdLevel += 1;
+    lcdLevel += 2;
   } else {
-    Serial.println("WAIT");
     currentLcdState = lcdWait;
   }
   analogWrite(lcd1Light, lcdLevel);
@@ -102,7 +104,6 @@ void lightUp() {
 
 void lightDown() {
   if (lcdLevel > 0) {
-    Serial.println("______");
     lcdLevel -= 1;
     currentLcdState = lcdDown;
   } else {
@@ -112,65 +113,117 @@ void lightDown() {
 }
 
 void adjustTemperature() {
-
   if (millis() - boilerOnTime < boilerStartDelay) {
     currentServoState = none;
     return;
   }
-  currentServoState = none;
-
+  //  currentServoState = none;
+  isCycleSkipped = false;
   if (currentTemp > (targetTemp + threshold) && servoAngle < minAngle) {
     int delta = 1;
-    if ((currentTemp + threshold -  targetTemp) > 2) {
+    if ((currentTemp + threshold - targetTemp) > 2) {
       delta = 2;
     }
-    if ((currentTemp + threshold -  targetTemp) > 4) {
+    if ((currentTemp + threshold - targetTemp) > 4) {
       delta = 3;
     }
-    if ((currentTemp + threshold -  targetTemp) > 9) {
+    if ((currentTemp + threshold - targetTemp) > 6) {
+      delta = 4;
+    }
+    if ((currentTemp + threshold - targetTemp) > 9) {
       delta = 5;
     }
+
+    if (
+      (delta >= 3 && (previousTemp - currentTemp) >= 1.2) ||
+      (delta < 2 && (previousTemp - currentTemp) >= 0.3)
+    ) {
+      isCycleSkipped = true;
+      previousTemp = currentTemp;
+      return;
+    } else {
+      isCycleSkipped = false;
+    }
+    previousTemp = currentTemp;
     servoAngle += delta;
     currentServoState = down;
   }
 
   if (currentTemp < (targetTemp - threshold) && servoAngle > maxAngle) {
     int delta = 1;
-    if ((currentTemp + threshold -  targetTemp) < -2) {
+    if ((currentTemp + threshold - targetTemp) < -2) {
       delta = 2;
     }
-    if ((currentTemp + threshold -  targetTemp) < -4) {
+    if ((currentTemp + threshold - targetTemp) < -4) {
       delta = 3;
     }
-    if ((currentTemp + threshold -  targetTemp) < -9) {
+    if ((currentTemp + threshold - targetTemp) < -6) {
+      delta = 4;
+    }
+    if ((currentTemp + threshold - targetTemp) < -9) {
       delta = 5;
     }
+
+    if (
+      (delta >= 3 && (previousTemp - currentTemp) <= -1.2) ||
+      (delta < 2 && (previousTemp - currentTemp) <= -0.5)
+    ) {
+      isCycleSkipped = true;
+      previousTemp = currentTemp;
+      return;
+    } else {
+      isCycleSkipped = false;
+    }
+    previousTemp = currentTemp;
     servoAngle -= delta;
     currentServoState = up;
   }
-
-  Serial.println(servoAngle);
   servo.write(servoAngle);
 }
 
 void getTemperature() {
   uint8_t i;
   float average;
+  uint16_t samples[NUMSAMPLES];
 
   for (i = 0; i < NUMSAMPLES; i++) {
     samples[i] = analogRead(THERMISTORPIN);
-    delay(10);
+    delay(3);
   }
 
   // average all the samples out
-  average = 0;
+  average = 0.0;
   for (i = 0; i < NUMSAMPLES; i++) {
     average += samples[i];
   }
   average /= NUMSAMPLES;
+  // TODO: add more diapasones
 
-  //42-195, 35 - 247, 48 - 156 53 - 137, 40-220
-  currentTemp = map(average, 247, 137, 35, 53);
+
+  // 33 - 265
+  // 35 - 247
+  // 36 - 238
+  // 38 - 220
+  // 42 - 195
+  // 48 - 156
+  // 53 - 137
+  if (average <= 156 && average >= 137) {
+    currentTemp = map(average, 156, 137, 48 * 10.0, 53 * 10) / 10.0;
+  } else if (average <= 195) {
+    currentTemp = map(average, 195, 156, 42 * 10.0, 48 * 10) / 10.0;
+  } else if (average <= 220) {
+    currentTemp = map(average, 220, 195, 38 * 10.0, 42 * 10) / 10.0;
+  } else if (average <= 238) {
+    currentTemp = map(average, 238, 220, 36 * 10.0, 38 * 10) / 10.0;
+  } else if (average <= 247) {
+    currentTemp = map(average, 247, 238, 35 * 10.0, 36 * 10) / 10.0;
+  } else if (average <= 265) {
+    currentTemp = map(average, 265, 247, 33 * 10.0, 35 * 10) / 10.0;
+  } else {
+    currentTemp = map(average, 265, 137, 33.0 * 10.0, 53.0 * 10.0) / 10.0;
+  }
+
+  //  currentTemp = map(average, 247, 137, 35.0, 53.0);
 }
 
 void checkIfBoilerOnline() {
@@ -188,21 +241,45 @@ void checkIfBoilerOnline() {
     isBoilerOnline = tempOnline;
     boilerOnTime = millis();
   }
+  if (!isBoilerOnline) {
+    previousTemp = 0.0;
+  }
+}
+
+void watchdogSetup(void) {
+  cli(); // disable all interrupts
+  wdt_reset(); // reset the WDT timer
+  /*
+    WDTCSR configuration:
+    WDIE = 1: Interrupt Enable
+    WDE = 1 :Reset Enable
+    WDP3 = 0 :For 2000ms Time-out
+    WDP2 = 1 :For 2000ms Time-out
+    WDP1 = 1 :For 2000ms Time-out
+    WDP0 = 1 :For 2000ms Time-out
+  */
+  // Enter Watchdog Configuration mode:
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+  //  WDTCSR |= (1 << WDE);
+  // Set Watchdog settings:
+  WDTCSR = (1 << WDIE) | (1 << WDE) | (0 << WDP3) | (1 << WDP2) | (1 << WDP1) | (1 << WDP0);
+  sei();
 }
 
 void setup()
 {
+
   pinMode(lcd1Light, OUTPUT);
   pinMode(boilerPin, INPUT);
 
   temperatureAdjustingThread.onRun(adjustTemperature);
-  temperatureAdjustingThread.setInterval(3000);
+  temperatureAdjustingThread.setInterval(5000);
 
   getCurrentTemperatureThread.onRun(getTemperature);
   getCurrentTemperatureThread.setInterval(500);
 
   boilerOnlineThread.onRun(checkIfBoilerOnline);
-  boilerOnlineThread.setInterval(100);
+  boilerOnlineThread.setInterval(200);
 
   lightUpThread.onRun(lightUp);
   lightUpThread.setInterval(1);
@@ -216,43 +293,43 @@ void setup()
   Serial.begin(9600);
   servo.attach(servoPin);
   isServoAttached = true;
-
+  servoAngle = minAngle;
   servo.write(servoAngle);
-  delay(1000);
-  servo.write(servoAngle);
-  delay(1000);
+  delay(2000);
+  servoAngle = startAngle;
   servo.write(servoAngle);
   lastActionTime = millis();
+  //   watchdogSetup();
 }
+
+//ISR(WDT_vect) // Watchdog timer interrupt. {
+// Include your code here - be careful not to use functions they may cause the interrupt to hang and
+// prevent a reset.
+//}
 
 void loop()
 {
-  Serial.print(currentLcdState);
-  Serial.println("___");
-  //  Serial.print(lcdLevel);
+  //    loop_count++;
+  //    wdt_reset();
 
-  if ((millis() - lastActionTime > lcdSleepDelay) && (lcdLevel >= 0) && (currentLcdState == lcdWait || currentLcdState == lcdDown)) {
+  if ((millis() - lastActionTime > lcdSleepDelay) /*&& (currentLcdState == lcdWait || currentLcdState == lcdDown)*/) {
     if (lightDownThread.shouldRun()) {
       lightDownThread.run();
-      Serial.println("Down");
     }
 
     if (isServoAttached) {
       servo.detach();
       isServoAttached = false;
     }
-  } else if ((millis() - lastActionTime < lcdSleepDelay) && (lcdLevel <= maxLevel) && (currentLcdState == lcdWait || currentLcdState == lcdUp)) {
+  } else if ((millis() - lastActionTime < lcdSleepDelay) /*&& (currentLcdState == lcdWait || currentLcdState == lcdUp)*/) {
     if (lightUpThread.shouldRun()) {
       lightUpThread.run();
-      Serial.println("Up");
     }
 
     if (!isServoAttached) {
       servo.attach(servoPin);
       isServoAttached = true;
     }
-
-
   }
 
   if (!isBoilerOnline) {
@@ -271,98 +348,6 @@ void loop()
     boilerOnlineThread.run();
 
   printToLcd1();
-  //275-25, 220 - 40
-  //  checkIfBoilerOnline();
-  //  temp = map(analogRead(THERMISTORPIN), 800, 635, 31, 51);
-
-
-
-
-
-  //  Serial.print("Average analog reading ");
-  //  Serial.println(average);
-
-  // convert the value to resistance
-  //  average = 1023 / average - 1;
-  //  average = SERIESRESISTOR / average;
-  //  //  Serial.print("Thermistor resistance ");
-  //  //  Serial.println(average);
-  //
-  //  float steinhart;
-  //  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
-  //  steinhart = log(steinhart);                  // ln(R/Ro)
-  //  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  //  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  //  steinhart = 1.0 / steinhart;                 // Invert
-  //  steinhart -= 273.15;                         // convert to C
-
-  //  Serial.print("Temperature ");
-  //  Serial.print(steinhart);
-  //  Serial.println(" *C");
-
-  //  delay(1000);
-
-
-
-  //  for (int i = 254; i >= 0; i--)
-  //  {
-  //    Serial.println(i);
-  //    analogWrite(lcd1Light, i);  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
-  //    delay(20);
-  //  }
-  //
-  //  for (int i = 1; i <= 255; i++)
-  //  {
-  //    Serial.println(i);
-  //    analogWrite(lcd1Light, i);  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
-  //    delay(20);
-  //  }
-
-
-
-
-  //  byte error, address;
-  //  int nDevices;
-  //
-  //  Serial.println("Scanning...");
-  //
-  //  nDevices = 0;
-  //  for(address = 1; address < 127; address++ )
-  //  {
-  //    // The i2c_scanner uses the return value of
-  //    // the Write.endTransmisstion to see if
-  //    // a device did acknowledge to the address.
-  //    Wire.beginTransmission(address);
-  //    error = Wire.endTransmission();
-  //
-  //    if (error == 0)
-  //    {
-  //      Serial.print("I2C device found at address 0x");
-  //      if (address<16)
-  //        Serial.print("0");
-  //      Serial.print(address,HEX);
-  //      Serial.println("  !");
-  //
-  //      nDevices++;
-  //    }
-  //    else if (error==4)
-  //    {
-  //      Serial.print("Unknown error at address 0x");
-  //      if (address<16)
-  //        Serial.print("0");
-  //      Serial.println(address,HEX);
-  //    }
-  //  }
-  //  if (nDevices == 0)
-  //    Serial.println("No I2C devices found\n");
-  //  else
-  //    Serial.println("done\n");
-  //
-  //  delay(5000);           // wait 5 seconds for next scan
-  //
-  //
-
-
 
   char key = kpd.getKey();
 
@@ -393,30 +378,47 @@ void loop()
 
 void printToLcd1() {
   String boiler = " ON ";
-  String servoString;
 
+  String emptyLeft = "       ";
+  String emptyRight = "       ";
+
+  String waitLeft = "    =  ";
+  String waitRight = "  =    ";
+
+  String goLeft = "    -> ";
+  String goRight = " <-    ";
+
+  String pausedLeft = "  ||-> ";
+  String pausedRight = " <-||  ";
+
+  lcd.home ();
   switch (currentServoState) {
     case none:
-      servoString = "    ";
+      lcd.print(waitLeft + String((int)targetTemp) + waitRight);
       break;
 
     case up:
-      servoString = " >> ";
+      if (isCycleSkipped) {
+        lcd.print(pausedLeft + String((int)targetTemp) + emptyRight);
+      } else {
+        lcd.print(goLeft + String((int)targetTemp) + emptyRight);
+      }
       break;
 
     case down:
-      servoString = " << ";
+      if (isCycleSkipped) {
+        lcd.print(emptyLeft + String((int)targetTemp) + pausedRight);
+      } else {
+        lcd.print(emptyLeft + String((int)targetTemp) + goRight);
+      }
       break;
   }
 
   if (!isBoilerOnline) {
     boiler = " OFF";
   }
-  lcd.home ();
-
-  lcd.print("Target = " + String(targetTemp));
   lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.print (String(currentTemp) + servoString + String(servoAngle) + boiler);
+  lcd.print (String(currentTemp) + "    " + String(180 - servoAngle) + boiler);
 }
 
 
